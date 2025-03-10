411a4e8d511994db7ec58b7c0f37cc3a73a55ed3
Mohamed Galal <mohamed754f@gmail.com>
2025-03-11 01:38:09 +0200

    [2942] [Time Beats: 100.00%] [Memory Beats: 32.49%] - LeetPush

--- Find-Words-Containing-Character.cpp
+++ Find-Words-Containing-Character.cpp
@@ -3,11 +3,8 @@ public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int>ans;
         for(int i=0;i<words.size();i++){
-            for(auto& word : words[i]){
-                if(word == x){
-                    ans.push_back(i);
-                    break;
-                }
+            if(words[i].find(x) != std::string::npos){
+                ans.push_back(i);
             }
         }
         return ans;
