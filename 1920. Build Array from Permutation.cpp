b777926c905a66da38a7685d50da619e77b4f387
Mohamed Galal <mohamed754f@gmail.com>
2025-05-05 22:04:53 +0300

    Update 1920. Build Array from Permutation.cpp

--- 1920. Build Array from Permutation.cpp
+++ 1920. Build Array from Permutation.cpp
@@ -1,4 +1,19 @@
 class Solution {
+        // Idea:
+        //  You are given a permutation array nums where each element is in the range [0, nums.length - 1]
+        //  You must return a new array such that: ans[i] = nums[nums[i]]
+        
+        // Edge Cases:
+        //  nums has only 1 element (e.g. [0])
+        //  nums is already in correct position (e.g. [0, 1, 2])
+        //  nums is reverse ordered (e.g. [2, 1, 0])
+        //  nums contains duplicate values (not applicable here, since it's a permutation by problem constraint)
+
+        // Approach:
+        //  Create a new array 'ans' of the same size as nums
+        //  Loop over nums from index 0 to n-1
+        //  For each index i, assign ans[i] = nums[nums[i]]
+        //  Return the array 'ans'
 public:
     vector<int> buildArray(vector<int>& nums) {
         vector<int> ans;
